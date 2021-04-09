c56969f1face1ba89a2f64a338a36b1771151d8a
ApplePie420 <radko27@centrum.sk>
2021-04-10 01:29:30 +0200

    new components - TI
    Regulator - LM117, LM338, LM350, TL783
    OpAmp - LM124, LM741 (changes)
    Comparator - LM111, LM311

--- Manufacturers/Texas_Instruments/General_Purpose_OpAmps.py
+++ Manufacturers/Texas_Instruments/General_Purpose_OpAmps.py
@@ -3,12 +3,25 @@ from _components.semiconductors.IC.analog.OpAmp import OpAmp
 class LM741A(OpAmp):
     name = "LM741"
     packages = ["TO-99", "CDIP", "PDIP"]
-    type = "general-purpose"
+    type = "dual general-purpose opamp"
     supplyVoltage = [22, "V"]
     differentialInputVoltage = [30, "V"]
-    maxOffsetVoltage = [0.003, "V"]
-    maxOffsetCurrent = [0.00003, "A"]
-    inputResistance = [6000000, "Ω"]
-    transientResponse = [0.0000008, "s"]
-    bandwidth = [1500000, "Hz"]
+    maxOffsetVoltage = [3, "mV"]
+    maxOffsetCurrent = [3, "uA"]
+    inputResistance = [6, "MΩ"]
+    transientResponse = [800, "ns"]
+    bandwidth = [1.5, "MHz"]
     slewRate = [0.7, "V/us"]
+
+class LM124(OpAmp):
+    name = "LM124"
+    packages = []
+    type = "quadruple general-purpose"
+    supplyVoltage = [16, "V"]
+    differentialInputVoltage = [32, "V"]
+    maxOffsetVoltage = [7, "mV"]
+    maxOffsetCurrent = [50, "A"]
+    inputResistance = None      # these two aren't specified in datasheet smh
+    transientResponse = None
+    bandwidth = [1.2, "MHz"]
+    slewRate = [0.5, "V/us"]
--- Manufacturers/Texas_Instruments/Linear_Voltage_Regulators.py
+++ Manufacturers/Texas_Instruments/Linear_Voltage_Regulators.py
@@ -7,6 +7,7 @@ class LM317(Regulator):
     maxInputVoltage = [40, "V"]
     refVoltage = [1.25, "V"]
     maxOutputVoltage = [maxInputVoltage[0] - refVoltage[0], "V"]
+    minOutputVoltage = [1.25, "V"]
     maxOutputCurrent = [1.5, "A"]
     minLoadCurrent = [0.004, "A"]
     currentLimit = True
@@ -30,19 +31,76 @@ class LM317(Regulator):
         return [power, "W"]
 
 class LM117():
-    None
+    name = "LM117"
+    type = "linear"
+    packages = ["TO-3", "TO"]
+    maxInputVoltage = [40, "V"]
+    refVoltage = [1.25, "V"]
+    maxOutputVoltage = [maxInputVoltage[0] - refVoltage[0], "V"]
+    minOutputVoltage = [1.25, "V"]
+    maxOutputCurrent = [1.5, "A"]
+    minLoadCurrent = [4, "mA"]
+    currentLimit = True
+    RMSnoise = [0.003, "%Vo"]
+    rippleRejection = [65, "dB"]
+    maxJunctionTemp = [125, "°C"]
 
 class LM338():
+    name = "LM338"
+    type = "linear"
+    packages = ["TO-220", "TO-CAN"]
+    maxInputVoltage = [40, "V"]
+    refVoltage = [1.24, "V"]
+    maxOutputVoltage = [maxInputVoltage[0] - refVoltage[0], "V"]
+    minOutputVoltage = [1.2, "V"]
+    maxOutputCurrent = [5, "A"]
+    minLoadCurrent = [5, "mA"]
+    currentLimit = [8, "A"]
+    RMSnoise = [0.003, "%Vo"]
+    rippleRejection = [75, "dB"]
+    maxJunctionTemp = [125, "°C"]
+    datasheet = "https://www.ti.com/lit/ds/symlink/lm338.pdf"
+    
     @staticmethod
     def Vout(R1, R2):
         return refVoltage*(1+(R2/R1))+(0.00005*R2)
 
 class LM350():
+    name = "LM338"
+    type = "linear"
+    packages = ["TO-220", "TO-CAN"]
+    maxInputVoltage = [33, "V"]
+    refVoltage = [1.25, "V"]
+    maxOutputVoltage = [maxInputVoltage[0] - refVoltage[0], "V"]
+    minOutputVoltage = [1.2, "V"]
+    maxOutputCurrent = [4.5, "A"]
+    minLoadCurrent = [3.5, "mA"]
+    currentLimit = [4.5, "A"]
+    RMSnoise = [0.001, "%Vo"]
+    rippleRejection = [86, "dB"]
+    maxJunctionTemp = [125, "°C"]
+    datasheet = "https://www.ti.com/lit/ds/snvs772b/snvs772b.pdf"
+
     @staticmethod
     def Vout(R1, R2):
         return refVoltage*(1+(R2/R1))+(0.00005*R2)
 
 class TL783():
+    name = "LM338"
+    type = "linear"
+    packages = ["TO-220", "TO-263", "PFM"]
+    maxInputVoltage = [125, "V"]
+    refVoltage = [1.27, "V"]
+    maxOutputVoltage = [maxInputVoltage[0] - refVoltage[0], "V"]
+    minOutputVoltage = [1.2, "V"]
+    maxOutputCurrent = [1100, "mA"]
+    minLoadCurrent = [15, "mA"]
+    currentLimit = [1100, "mA"]
+    RMSnoise = [0.003, "%Vo"]
+    rippleRejection = [76, "dB"]
+    maxJunctionTemp = [125, "°C"]
+    datasheet = "https://www.ti.com/lit/ds/symlink/tl783.pdf"
+
     @staticmethod
     def Vout(R1, R2):
         return refVoltage*(1+(R2/R1))+(0.00005*R2)
